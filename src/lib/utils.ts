1650b785c4d03ba55cff740634536f2b6a11c67e
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-02-21 18:02:37 +0100

    Features: Meta data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -28,4 +29,47 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice)
+}
+
+export function constructMetadata({
+  title = 'Video Streaming Marketplace',
+  description = 'Watch and learn from the best video tutorials on the internet.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@tutorialfromyt',
+    },
+    icons,
+    metadataBase: new URL('https://video-streaming-platform-on-next.vercel.app/'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
